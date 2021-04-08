ea0131178c7b8f423a8e477ebaa0fca3bb95cb75
Eve <e_sotnik@bk.ru>
2021-04-08 22:00:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DeclinationOfNumerals
+{
+    // метод, склоняющий существительное «рублей» следующее за указанным числительным.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PluralizeRubles(10));
+            Console.WriteLine(PluralizeRubles(1));
+            Console.WriteLine(PluralizeRubles(24));
+        }
+
+        public static string PluralizeRubles(int count)
+        {
+            string s = count.ToString();
+            int k = int.Parse(s.Substring(s.Length - 1));
+            int i = 0;
+            if (count > 10) i = int.Parse(s.Substring(s.Length - 2, 1));
+            if (i == 1) return "рублей";
+            if (k == 1) return "рубль";
+            else if (k == 2 || k == 3 || k == 4) return "рубля";
+            else return "рублей";
+        }
+    }
+}
